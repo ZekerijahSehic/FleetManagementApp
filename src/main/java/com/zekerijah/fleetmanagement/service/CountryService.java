411bdfa03c6bbfe6cb64feba3a05b61158cfc4f3
Zekerijah <zekerijahsehic1@gmail.com>
2021-10-15 00:43:17 +0200

    Display country list

--- src/main/java/com/zekerijah/fleetmanagement/service/CountryService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/CountryService.java
@@ -1,7 +1,21 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.Country;
+import com.zekerijah.fleetmanagement.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> getCountries(){
+        return countryRepository.findAll();
+    }
+
+
 }
