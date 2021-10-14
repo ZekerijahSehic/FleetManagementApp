7f2c7f500ab24faad855a11a627a4b122c754b79
Zekerijah <zekerijahsehic1@gmail.com>
2021-10-14 17:17:19 +0200

    Update model with JsonIdentityInfo

--- src/main/java/com/zekerijah/fleetmanagement/model/Vehicle.java
+++ src/main/java/com/zekerijah/fleetmanagement/model/Vehicle.java
@@ -1,5 +1,7 @@
 package com.zekerijah.fleetmanagement.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,7 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Vehicle {
 
     @Id
