557cb4f3f0a52931290e7c7142782b42fdd622a4
Zekerijah <zekerijahsehic1@gmail.com>
2021-10-15 02:45:22 +0200

    Display edit modal data

--- src/main/resources/static/js/country.js
+++ src/main/resources/static/js/country.js
@@ -1,5 +1,18 @@
-$('document').ready(function (){
-    $('#editButton').on('click', function (event){
+$('document').ready(function() {
+
+    $('table #editButton').on('click', function (event) {
+        event.preventDefault();
+
+        var href = $(this).attr('href');
+
+        $.get(href, function (country) {
+            $('#idEdit').val(country.id);
+            $('#countryEdit').val(country.name);
+            $('#capitalEdit').val(country.capital);
+            $('#codeEdit').val(country.code);
+            $('#continentEdit').val(country.continent);
+            $('#nationalityEdit').val(country.nationality);
+        });
         $('#editCountryModal').modal();
     })
 })
