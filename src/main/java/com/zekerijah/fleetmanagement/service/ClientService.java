e5be56ea8f6793b577e20e19a74db2a49f9ddb95
Zekerijah <zekerijahsehic1@gmail.com>
2021-10-21 15:31:48 +0200

    Client service methods

--- src/main/java/com/zekerijah/fleetmanagement/service/ClientService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/ClientService.java
@@ -1,12 +1,32 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.Client;
 import com.zekerijah.fleetmanagement.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
 
     @Autowired
     private ClientRepository clientRepository;
+
+    public List<Client> getClients(){
+        return clientRepository.findAll();
+    }
+
+    public void saveClient (Client client) {
+        clientRepository.save(client);
+    }
+
+    public Optional<Client> getClientById(Integer id){
+        return clientRepository.findById(id);
+    }
+
+    public void deleteClientById(Integer id) {
+        clientRepository.deleteById(id);
+    }
 }
