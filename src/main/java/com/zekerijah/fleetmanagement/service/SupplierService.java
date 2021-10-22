d90d140019e340f4b06513311f5b33ffc4ae7633
Zekerijah <zekerijahsehic1@gmail.com>
2021-10-22 19:36:13 +0200

    Crud methods

--- src/main/java/com/zekerijah/fleetmanagement/service/SupplierService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/SupplierService.java
@@ -1,7 +1,34 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.Supplier;
+import com.zekerijah.fleetmanagement.repository.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SupplierService {
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    public List<Supplier> getSuppliers(){
+        return supplierRepository.findAll();
+    }
+
+    public Optional<Supplier> getSupplierById(Integer id){
+        return supplierRepository.findById(id);
+    }
+
+    public void saveSupplier(Supplier supplier){
+        supplierRepository.save(supplier);
+    }
+
+    public void deleteSupplierById(Integer id){
+        supplierRepository.deleteById(id);
+    }
+
+
 }
