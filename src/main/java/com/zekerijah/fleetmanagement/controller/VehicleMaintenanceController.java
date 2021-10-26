a6648136c0845cfd05802e45440c4261dc9fbf14
Zekerijah <zekerijahsehic1@gmail.com>
2021-10-26 12:34:54 +0200

    Maintenance endpoints and methodes

--- src/main/java/com/zekerijah/fleetmanagement/controller/VehicleMaintenanceController.java
+++ src/main/java/com/zekerijah/fleetmanagement/controller/VehicleMaintenanceController.java
@@ -1,14 +1,66 @@
 package com.zekerijah.fleetmanagement.controller;
 
+import com.zekerijah.fleetmanagement.model.Contact;
+import com.zekerijah.fleetmanagement.model.Supplier;
+import com.zekerijah.fleetmanagement.model.Vehicle;
+import com.zekerijah.fleetmanagement.model.VehicleMaintenance;
+import com.zekerijah.fleetmanagement.service.SupplierService;
+import com.zekerijah.fleetmanagement.service.VehicleMaintenanceService;
+import com.zekerijah.fleetmanagement.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class VehicleMaintenanceController {
 
+    @Autowired
+    private VehicleMaintenanceService vehicleMaintenanceService;
+
+    @Autowired
+    private SupplierService supplierService;
+
+    @Autowired
+    private VehicleService vehicleService;
+
     @GetMapping("/vehicles-maintenances")
     public String getVehiclesMaintenances(Model model){
+        List<VehicleMaintenance> vehicleMaintenanceList = vehicleMaintenanceService.getVehicleMaintenances();
+        List<Supplier> supplierList = supplierService.getSuppliers();
+        List<Vehicle> vehicleList = vehicleService.getVehicles();
+
+        model.addAttribute("vehiclesMaintenances", vehicleMaintenanceList);
+        model.addAttribute("suppliers", supplierList);
+        model.addAttribute("vehicles", vehicleList);
+
         return "vehicle_maintenance";
+    }
+
+    @PostMapping("/vehicles-maintenances/add-new")
+    public String addVehicleMaintenance(VehicleMaintenance vehicleMaintenance){
+        vehicleMaintenanceService.saveVehicleMaintenance(vehicleMaintenance);
+        return "redirect:/vehicles-maintenances";
+    }
+
+    @RequestMapping("/vehicles-maintenances/find-by-id")
+    @ResponseBody
+    public Optional<VehicleMaintenance> getVehicleMaintenance(Integer id){
+        return vehicleMaintenanceService.getVehicleMaintenanceById(id);
+    }
+
+    @RequestMapping(value = "/vehicles-maintenances/update", method = {RequestMethod.GET, RequestMethod.PUT})
+    public String updateVehicleMaintenance(VehicleMaintenance vehicleMaintenance){
+        vehicleMaintenanceService.saveVehicleMaintenance(vehicleMaintenance);
+        return "redirect:/vehicles-maintenances";
+    }
+
+    @RequestMapping(value = "/vehicles-maintenances/delete", method = {RequestMethod.GET, RequestMethod.PUT})
+    public String deleteVehicleMaintenance(Integer id){
+        vehicleMaintenanceService.deleteVehicleMaintenanceById(id);
+        return "redirect:/vehicles-maintenances";
     }
 }
