a1196aa74ecb872f3c9f34257c2933ba26729538
Zekerijah <zekerijahsehic1@gmail.com>
2021-10-27 11:09:44 +0200

    Extend JpaRepository

---
+++ src/main/java/com/zekerijah/fleetmanagement/repository/VehicleHireRepository.java
@@ -0,0 +1,7 @@
+package com.zekerijah.fleetmanagement.repository;
+
+import com.zekerijah.fleetmanagement.model.VehicleHire;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleHireRepository extends JpaRepository<VehicleHire, Integer> {
+}
