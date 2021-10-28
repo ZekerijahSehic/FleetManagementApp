708cc78ad096105ad547fbd08c5ed5a37b9c2802
Zekerijah <zekerijahsehic1@gmail.com>
2021-10-28 11:36:54 +0200

    Employee crud

--- src/main/java/com/zekerijah/fleetmanagement/service/EmployeeService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/EmployeeService.java
@@ -1,7 +1,32 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.Employee;
+import com.zekerijah.fleetmanagement.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public void saveEmployee(Employee employee){
+        employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Integer id){
+        employeeRepository.deleteById(id);
+    }
+
+    public Optional<Employee> getEmployeById(Integer id){
+        return employeeRepository.findById(id);
+    }
 }
