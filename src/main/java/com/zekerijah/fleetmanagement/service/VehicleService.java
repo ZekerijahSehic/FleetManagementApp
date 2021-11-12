936955750e6b8eb98b947b5386eeacd569cf55c0
Zekerijah <zekerijahsehic1@gmail.com>
2021-11-12 11:01:56 +0100

    Crud methodes

--- src/main/java/com/zekerijah/fleetmanagement/service/VehicleService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/VehicleService.java
@@ -1,7 +1,32 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.Vehicle;
+import com.zekerijah.fleetmanagement.repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleService {
+
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    public List<Vehicle> getVehicles() {
+        return vehicleRepository.findAll();
+    }
+
+    public void saveVehicle(Vehicle vehicle) {
+        vehicleRepository.save(vehicle);
+    }
+
+    public Optional<Vehicle> getVehicleById(Integer id){
+        return vehicleRepository.findById(id);
+    }
+
+    public void deleteVehicle(Integer id){
+        vehicleRepository.deleteById(id);
+    }
 }
